5f93a8dd5dd590170046b7e8c8c7e6a7083229ed
Dexter Valencia <deeside@pacbell.net>
2020-02-14 00:21:47 -0800

    Updates

---
+++ precache-manifest.ccc887ee1b3eebcc4ae80457cea950cf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1afb65b3824ef5280f4bfa89d664061",
+    "url": "/unit19_State/index.html"
+  },
+  {
+    "revision": "29546691607c053c9064",
+    "url": "/unit19_State/static/css/main.03fc43db.chunk.css"
+  },
+  {
+    "revision": "37024b792e14b03e761f",
+    "url": "/unit19_State/static/js/2.118ed8fa.chunk.js"
+  },
+  {
+    "revision": "0bdc4f0d8b3d84ae6a73bb9902b479ee",
+    "url": "/unit19_State/static/js/2.118ed8fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29546691607c053c9064",
+    "url": "/unit19_State/static/js/main.bc516c45.chunk.js"
+  },
+  {
+    "revision": "28d02394fea5fbc8d172",
+    "url": "/unit19_State/static/js/runtime-main.3634df99.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/unit19_State/static/media/logo.5d5d9eef.svg"
+  }
+]);
