d6188306c412f4eb5b9fe91ca9ead780d7ba460f
Dexter Valencia <deeside@pacbell.net>
2020-02-18 13:08:52 -0800

    redeploy

--- server.js
+++ server.js
@@ -12,9 +12,9 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
+// if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
-}
+// }
 // Add routes, both API and view
 app.use(routes);
 
